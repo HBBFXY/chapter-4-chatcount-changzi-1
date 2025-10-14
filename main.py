1cfe83e863ee89931aa2093007720823f84aee5c
changzi-1 <2656748923@qq.com>
2025-10-14 19:44:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 s=input("请输入一行字符:")
 alpha=digit=space=other=0
 for char in s:
-    if char.isalpha() and (ord('a')<=ord(char)<=ord('z') or ord('A')<=ord(char)<=ord('Z')):
+    if char.isalpha():
          alpha += 1
     elif char.isdigit():
          digit += 1
