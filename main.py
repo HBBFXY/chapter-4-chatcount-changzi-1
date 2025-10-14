30f317b2076919f5c715e7293048d5aec97488db
changzi-1 <2656748923@qq.com>
2025-10-14 20:20:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-s=input("请输入一行字符:")
-alpha=digit=space=other=0
+
+s = input("请输入一行字符：")
+alpha = digit = space = other = 0
 for char in s:
     if char.isalpha():
-         alpha += 1
+        alpha += 1
     elif char.isdigit():
-         digit += 1
-    elif char ==' ':
-         space += 1
+        digit += 1
+    elif char == ' ':
+        space += 1
     else:
-         other += 1
-print("英文字符:",alpha)
-print("数字:",digit)
-print("空格:",space)
-print("其他字符:",other)
+        other += 1
+print("英文字符：", alpha)
+print("数字：", digit)
+print("空格：", space)
+print("其他字符：", other)
