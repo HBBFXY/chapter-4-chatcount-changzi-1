5a64c27f847e9f985fb2f2432d93bd363e8e1a24
changzi-1 <2656748923@qq.com>
2025-10-14 19:35:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# 这个文件用于编写代码
+s=input("请输入一行字符:")
+alpha=digit=space=other=0
+for char in s:
+    if char.isalpha() and (ord('a')<=ord(char)<=ord('z') or ord('A')<=ord(char)<=ord('Z')):
+         alpha += 1
+    elif char.isdigit():
+         digit += 1
+    elif char =='':
+        space += 1
+    else:
+        other += 1
+print("英文字符:",alpha)
+print("数字:",digit)
+print("空格:",space)
+print("其他字符:",other)
